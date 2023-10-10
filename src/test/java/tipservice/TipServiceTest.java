3a87d0b097098a8e16463a617d6f10e924120426
PatesM <maximpates@gmail.com>
2023-10-10 12:07:26 +0500

    task completion

--- src/test/java/tipservice/TipServiceTest.java
+++ src/test/java/tipservice/TipServiceTest.java
@@ -1,27 +1,34 @@
 package tipservice;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.maxqa.TipService;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.math.BigDecimal;
-
 public class TipServiceTest {
 
-    @ParameterizedTest(name = "Test #{index}: amount equals - {0}")
-    @CsvSource({
-            "-1, -1,1",
-            "0, 0",
-            "500, 550",
-            "999, 1098,9",
-            "1000, 1050",
-            "1001, 1051,05"
-    })
-    public void tipServiceTest_shouldRoundTen(BigDecimal amount, BigDecimal expected) {
+    static Stream<Arguments> tipService_argsProviderFactory() {
+        return Stream.of(
+                Arguments.of(-1, -1.10),
+                Arguments.of(0, 0.00),
+                Arguments.of(999, 1098.90),
+                Arguments.of(1000, 1050.00),
+                Arguments.of(1001, 1051.05)
+        );
+    }
+
+    @ParameterizedTest(name = "Test #{index}: amount1 is - {0}")
+    @MethodSource("tipService_argsProviderFactory")
+    public void tipServiceTest_shouldRoundTen(double amountDouble, double expectedDouble) {
         TipService tipService = new TipService();
-        BigDecimal result = tipService.roundTips(amount);
-        assertEquals(result, result);
+        BigDecimal resultBigDecimal = tipService.roundTips(BigDecimal.valueOf(amountDouble));
+        String result = String.format("%.2f", resultBigDecimal);
+        String expected = String.format("%.2f", expectedDouble);
+        assertEquals(expected, result);
     }
 }
